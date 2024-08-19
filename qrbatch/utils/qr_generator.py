94a4ffa1f2a03ad858823c54ead295d9ebe877cc
PoHan Lin <pohan_lin@brightestTECH.com>
2024-08-19 10:40:30 +0800

    Add debugger file launch.json. Edit  custom_config.ini, excel column name. Edit README.md Edit qr_generator.py split process_excel function to sheet and row. Add row value validation is nan will not process.

--- qrbatch/utils/qr_generator.py
+++ qrbatch/utils/qr_generator.py
@@ -53,7 +53,12 @@ class QRCodeGenerator:
 
     def generate_qr_code(self, data: str, filename: str) -> None:
         """Generate QR code and save as an image file"""
-        qr = qrcode.QRCode(version=None, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+        qr = qrcode.QRCode(
+            version=None, 
+            error_correction=qrcode.constants.ERROR_CORRECT_L, 
+            box_size=10, 
+            border=4
+        )
         qr.add_data(data)
         qr.make(fit=True)
         img = qr.make_image(fill_color="black", back_color="white")
@@ -67,42 +72,59 @@ class QRCodeGenerator:
         """Process Excel file and generate QR code for each row in each sheet"""
         
         logging.info(f"Running QR Code Generator version {self.version}")
-       
+        
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+        
         try:
             xls = pd.ExcelFile(self.excel_file)
-            sheets_to_process = set(self.sheets) & set(xls.sheet_names)
             
-            try:
-                row_header = int(self.row_header[0]) if self.row_header else None
-            except ValueError:
-                logging.error(f"Invalid row header value: {self.row_header}")
-                raise ValueError(f"Invalid row header: {self.row_header} is not an integer.")
-
-            if not os.path.exists(self.output_folder):
-                os.makedirs(self.output_folder)
+            if self.sheets:
+                sheets_to_process = set(self.sheets) & set(xls.sheet_names)
+            else:
+                sheets_to_process = xls.sheet_names
+            
+            row_header = None
+            if self.row_header:
+                try:
+                    row_header = int(self.row_header[0])
+                except ValueError:
+                    logging.error(f"Invalid row header value: {self.row_header}")
+                    raise ValueError(f"Invalid row header: {self.row_header} is not an integer.")
 
             for sheet_name in sheets_to_process:
-                try:
-                    df = pd.read_excel(xls, sheet_name=sheet_name, header=row_header)
-                    logging.info(f"Columns in sheet '{sheet_name}': {df.columns.tolist()}")
-                    df = self.filter_columns(df)
-                    logging.info(f"Columns after filtering: {df.columns.tolist()}")
-
-                    sheet_folder = os.path.join(self.output_folder, sheet_name)
-                    if not os.path.exists(sheet_folder):
-                        os.makedirs(sheet_folder)
-
-                    for index, row in df.iterrows():
-                        formatted_data = self.format_data(row)
-                        item_identifier = self.clean_filename(str(row.iloc[0]))
-                        qr_filename = os.path.join(sheet_folder, f"qr_{sheet_name}_item_{item_identifier}.png")
-                        self.generate_qr_code(formatted_data, qr_filename)
-                        logging.info(f"Generated QR code: {qr_filename}")
-
-                except Exception as e:
-                    logging.error(f"Error processing sheet '{sheet_name}': {str(e)}")
-                    raise
+                self.process_sheet(xls, sheet_name, row_header)
 
         except Exception as e:
             logging.error(f"Error processing Excel file: {str(e)}")
             raise
+
+    def process_sheet(self, xls, sheet_name, row_header):
+        try:
+            df = pd.read_excel(xls, sheet_name=sheet_name, header=row_header)
+            logging.info(f"Columns in sheet '{sheet_name}': {df.columns.tolist()}")
+            df = self.filter_columns(df)
+            logging.info(f"Columns after filtering: {df.columns.tolist()}")
+
+            sheet_folder = os.path.join(self.output_folder, sheet_name)
+            if not os.path.exists(sheet_folder):
+                os.makedirs(sheet_folder)
+
+            for index, row in df.iterrows():
+                self.process_row(row, index, sheet_name, sheet_folder)
+
+        except Exception as e:
+            logging.error(f"Error processing sheet '{sheet_name}': {str(e)}")
+            raise
+
+    def process_row(self, row, index, sheet_name, sheet_folder):
+        item_identifier = self.clean_filename(str(row.iloc[0]))
+        
+        if pd.isna(item_identifier) or item_identifier.lower() == 'nan':
+            logging.warning(f"Skipped row at index {index} due to NaN identifier.")
+            return
+        
+        formatted_data = self.format_data(row)
+        qr_filename = os.path.join(sheet_folder, f"qr_{sheet_name}_item_{index}.png")
+        self.generate_qr_code(formatted_data, qr_filename)
+        logging.info(f"Generated QR code: {qr_filename}")
